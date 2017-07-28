aff3443f3f7d5b5f934d44f25712f70a72a9a5b5
Japson <jjacobj@cisco.com>
2017-07-27 23:57:27 -0700

    Barebone items

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# jjacobj84.github.io
-CSC 498 Data Visualization final project
+
---
+++ js/barChart.js
@@ -0,0 +1,38 @@
+function drawWorldDataChart(){
+    var opt = {
+      "actions": false,
+      "mode": "vega-lite"
+    }
+    var spec = {    
+        "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+        "data": {"url": "data/SummaryData.csv"},
+        "mark": "bar",
+        "width" : "300",
+        "height" : "300",
+        "encoding": {
+            "x": {"aggregate": "sum", "field": "Victims Count", "type": "quantitative"},
+            "y": {"field": "Region", "type": "nominal"},
+            "color": {"field": "Victims", "type": "nominal"},
+        }
+    };
+    
+    vega.embed("#worldSummarybarChar", spec, opt, function(error, result) {
+    var tooltipOption = {
+        showAllFields: false,
+        fields: [
+        {
+            field: "Victims",
+            title: "Type",
+            formatType : "string"
+        },
+        {
+            field: "Victims Count",
+            title: "Count",
+            formatType : "number"
+        }
+        ],
+        delay: 50
+    };
+    vegaTooltip.vegaLite(result.view, spec,tooltipOption);
+  });
+}
